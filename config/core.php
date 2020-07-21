583ac696e7dbea7f3d8d8b713cfd0934b75d6f5d
Alexandr Lomakin <sapsan1105@gmail.com>
2020-07-21 12:34:42 +0300

    i1

---
+++ config/core.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    require(ROOT . "config/db.php");
+    require(ROOT . "core/Model.php");
+    require(ROOT . "core/Controller.php");
---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+class Database
+{
+    private static $bdd = null;
+
+    private function __construct() {
+    }
+
+    public static function getBdd() {
+        if(is_null(self::$bdd)) {
+            self::$bdd = new PDO("mysql:host=localhost;dbname=tasks", 'tp', '12345');
+        }
+        return self::$bdd;
+    }
+}
